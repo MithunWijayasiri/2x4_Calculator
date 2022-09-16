dfd9790055580b270c67fea5bd392ca1e372cb4b
Mithun Wijayasiri <mithunedu1@gmail.com>
2022-09-16 13:24:18 +0530

    Commit

---
+++ app/src/main/java/com/example/calculatortwovalues/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.calculatortwovalues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //    Add Operation
+    @SuppressLint("SetTextI18n")
+    public void Add(View v)
+    {
+//        Variable Declaration
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+
+//        Covert variable into integer
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et2.getText().toString());
+        int result = n1 + n2;
+
+//        Display Result
+        et3.setText("Total Value " + result);
+    }
+
+    //    Subtract Operation
+    @SuppressLint("SetTextI18n")
+    public void Subtract(View v)
+    {
+
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et2.getText().toString());
+//      Only change here!
+        int result = n1 - n2;
+
+//      Display Result
+        et3.setText("Subtract Value " + result);
+    }
+
+        //    Multiply Operation
+    @SuppressLint("SetTextI18n")
+    public void Multiply(View v)
+    {
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et2.getText().toString());
+//       Only change here!
+        int result = n1 * n2;
+
+//        Display Result
+        et3.setText("Multiply Value " + result);
+    }
+
+        //    Divide Operation
+    @SuppressLint("SetTextI18n")
+    public void Divide(View v)
+    {
+        EditText et1=(EditText)findViewById(R.id.editTextNumber);
+        EditText et2=(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3=(EditText)findViewById(R.id.editTextNumber3);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et2.getText().toString());
+//       Only change here!
+        int result = n1 / n2;
+
+//        Display Result
+        et3.setText("Divide Value " + result);
+    }
+}
