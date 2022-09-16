7dd088c80fa1da4803030a86b5eac2c89e8eca48
Trishiraj <gamerprince.exp@gmail.com>
2022-09-16 16:50:02 +0530

    Simplify and process logic properly
    Also added a clear results button

--- app/src/main/java/com/example/calculatortwovalues/MainActivity.java
+++ app/src/main/java/com/example/calculatortwovalues/MainActivity.java
@@ -10,68 +10,87 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 @SuppressLint("SetTextI18n")
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText et1, et2;
     private TextView tv;
+    private Button addButton, multiplyButton, divideButton, subButton, clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //init Edit text fields (input)
         et1 = findViewById(R.id.editTextNumber);
         et2 = findViewById(R.id.editTextNumber2);
+
+        //init output textview
         tv = findViewById(R.id.editTextNumber3);
 
+        // init buttons
+        addButton = findViewById(R.id.buttonAdd);
+        addButton.setOnClickListener(this);
+        multiplyButton = findViewById(R.id.buttonMultiply);
+        multiplyButton.setOnClickListener(this);
+        divideButton = findViewById(R.id.buttonDivide);
+        divideButton.setOnClickListener(this);
+        subButton = findViewById(R.id.buttonSubtract);
+        subButton.setOnClickListener(this);
+        clearButton = findViewById(R.id.buttonClear);
+        clearButton.setOnClickListener(this);
     }
 
-    //    Add Operation
-
-    public void Add(View v)
+    private void Add()
     {
-//        Covert variable into integer
         int n1 = Integer.parseInt(et1.getText().toString());
         int n2 = Integer.parseInt(et2.getText().toString());
         int result = n1 + n2;
 
-//        Display Result
         tv.setText("Total Value " + result);
     }
 
-    //    Subtract Operation
-    public void Subtract(View v)
+    private void Subtract()
     {
         int n1 = Integer.parseInt(et1.getText().toString());
         int n2 = Integer.parseInt(et2.getText().toString());
-//      Only change here!
         int result = n1 - n2;
 
-//      Display Result
         tv.setText("Subtract Value " + result);
     }
 
-        //    Multiply Operation
-    public void Multiply(View v)
+    private void Multiply()
     {
         int n1 = Integer.parseInt(et1.getText().toString());
         int n2 = Integer.parseInt(et2.getText().toString());
-//       Only change here!
         int result = n1 * n2;
 
-//        Display Result
         tv.setText("Multiply Value " + result);
     }
 
-        //    Divide Operation
-    public void Divide(View v)
+    private void Divide()
     {
         int n1 = Integer.parseInt(et1.getText().toString());
         int n2 = Integer.parseInt(et2.getText().toString());
-//       Only change here!
         int result = n1 / n2;
 
-//        Display Result
         tv.setText("Divide Value " + result);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == addButton) {
+            Add();
+        } else if (v == multiplyButton) {
+            Multiply();
+        } else if (v == divideButton) {
+            Divide();
+        } else if (v == subButton) {
+            Subtract();
+        } else if (v == clearButton) {
+            tv.setText("");
+            et1.getText().clear();
+            et2.getText().clear();
+        }
     }
 }
